3bd82e8d6f78022ddf76bafd7d22624c7caf755e
Phil Curzon <phil@novelfs.org>
2017-04-25 22:59:57 +0100

    Fixed test dependencies and added a couple of tests

--- tests/CSharp.Formatting.Tests/Tests.fs
+++ tests/CSharp.Formatting.Tests/Tests.fs
@@ -1,8 +1,21 @@
-module CSharp.Formatting.Tests
+namespace CSharp.Formatting.Tests
 
+open FsCheck
+open FsCheck.Xunit
 open CSharp.Formatting
-open NUnit.Framework
 
-[<Test>]
-let ``hello returns 42`` () =
-  ()
+type ``Html Generator Tests``() =
+    [<Property>]
+    member __.``Given a tag name, tag class and empty content a matching html tag is created`` (tagName : NonEmptyString, tagClass : NonEmptyString) =
+        let html = HtmlGenerator.createTag tagName.Get tagClass.Get None (System.String.Empty)
+        html = sprintf """<%s class="%s"></%s>""" tagName.Get tagClass.Get tagName.Get
+
+    [<Property>]
+    member __.``Given a tag name, tag class and content a matching html tag is created`` (tagName : NonEmptyString, tagClass : NonEmptyString, content : NonNull<string>) =
+        let html = HtmlGenerator.createTag tagName.Get tagClass.Get None (content.Get)
+        html = sprintf """<%s class="%s">%s</%s>""" tagName.Get tagClass.Get content.Get tagName.Get
+
+    [<Property>]
+    member __.``Given a tag name, tag class, title and content a matching html tag is created`` (tagName : NonEmptyString, tagClass : NonEmptyString, tagTitle : NonEmptyString, content : NonNull<string>) =
+        let html = HtmlGenerator.createTag tagName.Get tagClass.Get (Some tagTitle.Get) (content.Get)
+        html = sprintf """<%s class="%s" title="%s">%s</%s>""" tagName.Get tagClass.Get tagTitle.Get content.Get tagName.Get
